736406b71b4e8d162baa485c05c56a98c62e6a4e
Mark Mayer <mdm373@gmail.com>
2019-12-19 06:40:15 +0000

    removing unused script

--- scripts/get-data.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d ".dat" ]; then
-    mkdir  .dat
-fi
-sh ./scripts/download-from.sh nypd-complaint-historical qgea-i56i
-sh ./scripts/download-from.sh  nypd-complaint-ytd 5uac-w243
-sh ./scripts/download-from.sh  nypd-sectors 5rqd-h5ci
-sh ./scripts/download-from.sh  community-districts jp9i-3b7y
-
-
